917c564f8d88bb254b8ad2933374da96d6e3d0c6
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-14 13:21:51 +0300

    Add navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles/main.css">
     <title>Document</title>
 </head>
 <body>
-    Index
+    <nav class="navbar">
+        <div class="container">
+            <a class="navbar__link" href="#">Главная</a>
+            <a class="navbar__link" href="#">Вход</a>
+            <a class="navbar__link" href="#">Регистрация</a>
+        </div>
+    </nav>
 </body>
 </html>
