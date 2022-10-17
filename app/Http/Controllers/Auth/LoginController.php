8626769ad7626aadf78da0e626143651adddfb79
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-17 11:47:27 +0300

    Added redirect to admin if user role is admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,15 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/home';
+
+    protected function redirectTo() {
+        if (auth()->user()->role == 'admin') {
+            return '/admin';
+        }
+
+        return '/home';
+    }
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,10 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [PostController::class, 'index'])->name('index');
 Auth::routes();
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/{post}', [PostController::class, 'detail'])->name('detail');
